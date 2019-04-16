82d30bc8b7cf173904096536e120014ba995b059
swan.kim <swan.kim@kakaocorp.com>
2019-04-17 00:52:28 +0900

    Updates

---
+++ precache-manifest.2af4582bddc08ffb08b4a7c702087231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/whichwhich/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "168e971174035394ce48",
+    "url": "/whichwhich/static/js/runtime~main.be908a74.js"
+  },
+  {
+    "revision": "6c661e3e56f0dd46172c",
+    "url": "/whichwhich/static/js/main.65db79f2.chunk.js"
+  },
+  {
+    "revision": "997a1fedf7aed5bd24f4",
+    "url": "/whichwhich/static/js/2.e55968e2.chunk.js"
+  },
+  {
+    "revision": "6c661e3e56f0dd46172c",
+    "url": "/whichwhich/static/css/main.c85ec9a1.chunk.css"
+  },
+  {
+    "revision": "dca78181c75de75278e3fe9423741997",
+    "url": "/whichwhich/index.html"
+  }
+];
