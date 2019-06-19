2cecd3184b858b8fda3f3abd85023585a46a16a7
swan.kim <swan.kim@kakaocorp.com>
2019-06-19 21:40:01 +0900

    blue dot section

--- src/Component/WhichMap.js
+++ src/Component/WhichMap.js
@@ -4,6 +4,13 @@ import redDot2 from '../images/red_dot2.png';
 import redDot3 from '../images/red_dot3.png';
 import redDot4 from '../images/red_dot4.png';
 import redDot5 from '../images/red_dot5.png';
+import blueDot1 from '../images/blue_dot.png';
+import blueDot2 from '../images/blue_dot2.png';
+import blueDot3 from '../images/blue_dot3.png';
+import blueDot4 from '../images/blue_dot4.png';
+import blueDot5 from '../images/blue_dot5.png';
+
+
 
 
 const imageSize = new window.daum.maps.Size(18, 20);
@@ -13,15 +20,36 @@ function getCoordsHash(coords) {
   return coords.ib * 3 + coords.jb * 1004;
 }
 
-function getMarkerImageSource(count) {
-  if (count === 2) {
-    return redDot2;
-  } else if (count === 3) {
-    return redDot3;
-  } else if (count === 4) {
-    return redDot4;
-  } else {
-    return redDot5;
+function getMarkerImageBase(index) {
+  if (index === 0) {
+    return redDot1;
+  } else if (index === 1) {
+    return blueDot1;
+  }
+
+}
+
+function getMarkerImageSource(index, count) {
+  if (index === 0) {
+    if (count === 2) {
+      return redDot2;
+    } else if (count === 3) {
+      return redDot3;
+    } else if (count === 4) {
+      return redDot4;
+    } else {
+      return redDot5;
+    }
+  } else if (index === 1) {
+    if (count === 2) {
+      return blueDot2;
+    } else if (count === 3) {
+      return blueDot3;
+    } else if (count === 4) {
+      return blueDot4;
+    } else {
+      return blueDot5;
+    }
   }
 }
 
@@ -32,6 +60,9 @@ export default class WhichMap extends Component{
     this.handleMapClick = this.handleMapClick.bind(this);
     this.searchAddress = this.searchAddress.bind(this);
     this.handleInfoChange1 = this.handleInfoChange1.bind(this);
+    this.handleInfoChange2 = this.handleInfoChange2.bind(this);
+    this.handleInfoChange3 = this.handleInfoChange3.bind(this);
+    this.handleInfoChange4 = this.handleInfoChange4.bind(this);
     this.handleAddressChange = this.handleAddressChange.bind(this);
     this.handleDrawMarker = this.handleDrawMarker.bind(this);
 
@@ -169,74 +200,104 @@ export default class WhichMap extends Component{
 
   handleDrawMarker(e) {
     e.preventDefault();
-    const { daumMap, info1, geocoder, markerImage } = this.state;
+    const { daumMap, info1, info2, geocoder, markerImage } = this.state;
 
     console.log(info1);
+    console.log(info2);
     const addressList = info1.split(',');
+    const addressList2 = info2.split(',');
     console.log(addressList, 'addressList');
     const errorList = [];
     // console.log(markerImage, 'image');
     const history = new Array();
     var coordsMap = new Map();
+    const hop = Array.of(addressList, addressList2);
+    console.log(hop);
     coordsMap.set('hello', 'bonjour');
 
-
-    let processes = addressList.map((address) => {
+    let processes = hop.map((addressList, index) => {
       return new Promise((resolve) => {
-        geocoder.addressSearch(address, function(result, status) {
-          if (status === window.daum.maps.services.Status.OK) {
-            let coords = new window.daum.maps.LatLng(result[0].y, result[0].x);
-            if (history.includes(coords.ib)) {
-              console.log('중복발견');
-            }
-            console.log(coords.ib, 'coords');
+        addressList.forEach(address => {
+          geocoder.addressSearch(address, function(result, status) {
+            if (status === window.daum.maps.services.Status.OK) {
+              let coords = new window.daum.maps.LatLng(result[0].y, result[0].x);
+    
+              let tmpMarker = {};
   
-            let tmpMarker = {};
-            // if (history.includes(getCoordsHash(coords))) {
-            //   console.log("중복발견");
-            //   const newMarkerImage = new window.daum.maps.MarkerImage(redDot2, imageSize, imageOption);
-              
-            //   tmpMarker = new window.daum.maps.Marker({
-            //     position: coords,
-            //     image: newMarkerImage
-            //   });
-            // } else {
-            //   history.push(getCoordsHash(coords));
-            //   tmpMarker = new window.daum.maps.Marker({
-            //     position: coords,
-            //     image: markerImage
-            //   });
-            // }
-
-            if (coordsMap.get(getCoordsHash(coords)) !== undefined) {
-              console.log("중복발견");
-
-              var newCount = coordsMap.get(getCoordsHash(coords)) + 1;
-              coordsMap.set(getCoordsHash(coords), newCount);
-              const newMarkerImage = new window.daum.maps.MarkerImage(getMarkerImageSource(newCount), imageSize, imageOption);
-              
-              tmpMarker = new window.daum.maps.Marker({
-                position: coords,
-                image: newMarkerImage
-              });
+              if (coordsMap.get(getCoordsHash(coords)) !== undefined) {
+                console.log("중복발견");
+  
+                var newCount = coordsMap.get(getCoordsHash(coords)) + 1;
+                coordsMap.set(getCoordsHash(coords), newCount);
+                const newMarkerImage = new window.daum.maps.MarkerImage(getMarkerImageSource(index, newCount), imageSize, imageOption);
+                
+                tmpMarker = new window.daum.maps.Marker({
+                  position: coords,
+                  image: newMarkerImage
+                });
+              } else {
+                coordsMap.set(getCoordsHash(coords), 1);
+                const markerImageBase = new window.daum.maps.MarkerImage(getMarkerImageBase(index), imageSize, imageOption);
+                tmpMarker = new window.daum.maps.Marker({
+                  position: coords,
+                  image: markerImageBase
+                });
+              }
+    
+              tmpMarker.setMap(daumMap);
+              resolve();
             } else {
-              coordsMap.set(getCoordsHash(coords), 1);
-              tmpMarker = new window.daum.maps.Marker({
-                position: coords,
-                image: markerImage
-              });
+              console.log(address, "실패");
+              errorList.push(address);
+              resolve();
             }
-  
-            tmpMarker.setMap(daumMap);
-            resolve();
-          } else {
-            console.log(address, "실패");
-            errorList.push(address);
-            resolve();
-          }
-        });
+          });
+        })
       })
     });
+
+
+    // let processes = addressList.map((address) => {
+    //   return new Promise((resolve) => {
+    //     geocoder.addressSearch(address, function(result, status) {
+    //       if (status === window.daum.maps.services.Status.OK) {
+    //         let coords = new window.daum.maps.LatLng(result[0].y, result[0].x);
+    //         if (history.includes(coords.ib)) {
+    //           console.log('중복발견');
+    //         }
+    //         console.log(coords.ib, 'coords');
+  
+    //         let tmpMarker = {};
+
+    //         if (coordsMap.get(getCoordsHash(coords)) !== undefined) {
+    //           console.log("중복발견");
+
+    //           var newCount = coordsMap.get(getCoordsHash(coords)) + 1;
+    //           coordsMap.set(getCoordsHash(coords), newCount);
+    //           const newMarkerImage = new window.daum.maps.MarkerImage(getMarkerImageSource(newCount), imageSize, imageOption);
+              
+    //           tmpMarker = new window.daum.maps.Marker({
+    //             position: coords,
+    //             image: newMarkerImage
+    //           });
+    //         } else {
+    //           coordsMap.set(getCoordsHash(coords), 1);
+    //           tmpMarker = new window.daum.maps.Marker({
+    //             position: coords,
+    //             image: markerImage
+    //           });
+    //         }
+  
+    //         tmpMarker.setMap(daumMap);
+    //         resolve();
+    //       } else {
+    //         console.log(address, "실패");
+    //         errorList.push(address);
+    //         resolve();
+    //       }
+    //     });
+    //   })
+    // });
 
     Promise.all(processes).then(() => {
       console.log(history, 'history');
